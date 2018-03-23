6670f03432ffcfbfd7312e438170408888df80fe
0ZeroZeus <0ZeroZeus@gmail.com>
2018-03-23 00:16:51 -0300

    Update Game.h

--- Trabalho1/include/Game.h
+++ Trabalho1/include/Game.h
@@ -7,9 +7,9 @@ using namespace std;
 
 class Game{
     public:
-        static Game* GetInstance();
-        SDL_Renderer* GetRenderer();
-        State* GetState();     
+        static Game* GetInstance();   /*Na especificação, esses dois métodos retornariam referencias ao invés de ponteiros,*/
+        State* GetState();           /*porem eu os fiz assim para que ficasse mais confortavel para eu trabalhar no codigo*/
+        SDL_Renderer* GetRenderer();   
 
         ~Game();
         void Run();
